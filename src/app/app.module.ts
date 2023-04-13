a1d6880868f3a78c8b22f516b037c9e23329d6b7
Serhii Hultiaieev <serhii_hultiaiev@epam.com>
2023-04-13 21:13:23 +0200

    Give visual feedback on favourites list manipulations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
 import { persistState } from '@datorama/akita';
+import {
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  MatSnackBarModule,
+} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,12 +24,19 @@ import { persistState } from '@datorama/akita';
     MatToolbarModule,
     MatButtonModule,
     HttpClientModule,
+    MatSnackBarModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
   ],
   providers: [
     {
       provide: 'persistState',
       useValue: persistState(),
+    },
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        duration: 1500,
+      },
     },
   ],
   bootstrap: [AppComponent],
--- src/app/shared/state/favourite-photos/favourite-photos.service.ts
+++ src/app/shared/state/favourite-photos/favourite-photos.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { FavouritePhotosStore } from './favourite-photos.store';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class FavouritePhotosService {
-  constructor(private favouritePhotosStore: FavouritePhotosStore) {}
+  constructor(
+    private readonly favouritePhotosStore: FavouritePhotosStore,
+    private readonly matSnackBar: MatSnackBar
+  ) {}
 
   add(imgUrl: string) {
     this.favouritePhotosStore.update((state) => {
       state[imgUrl] = true;
     });
+    this.matSnackBar.open('Image has been added to the favorites list');
   }
 
   remove(imgUrl: string) {
     this.favouritePhotosStore.update((state) => {
       delete state[imgUrl];
     });
+    this.matSnackBar.open('Image has been removed from the favorites list');
   }
 }
